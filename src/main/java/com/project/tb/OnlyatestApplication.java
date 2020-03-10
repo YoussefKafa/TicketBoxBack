801aff6804cd3e935eb00d52bbc6bb3d41cabeda
YoussefKafa <48362996+YoussefKafa@users.noreply.github.com>
2020-03-11 00:19:24 +0200

    update1

--- src/main/java/com/project/tb/OnlyatestApplication.java
+++ src/main/java/com/project/tb/OnlyatestApplication.java
@@ -1,16 +1,18 @@
 package com.project.tb;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @SpringBootApplication
-public class TbApplication {
+@EnableJpaAuditing
+public class OnlyatestApplication {
 	@Bean
 	BCryptPasswordEncoder bCryptPasswordEncoder(){
 		//we've created this bean here so we can autowired it with user services
-		return new BCryptPasswordEncoder();
-	}
+		return new BCryptPasswordEncoder();}
 	public static void main(String[] args) {
-		SpringApplication.run(TbApplication.class, args);
+		SpringApplication.run(OnlyatestApplication.class, args);
 	}
 }
--- src/main/java/com/project/tb/services/UserServices.java
+++ src/main/java/com/project/tb/services/UserServices.java
@@ -1,7 +1,9 @@
 package com.project.tb.services;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
