0f1cd446653a43708e7cf462ef737744c1705dec
YoussefKafa <48362996+YoussefKafa@users.noreply.github.com>
2020-09-14 14:52:01 +0300

    Update QRCodeServices.java

--- src/main/java/com/project/tb/services/QRCodeServices.java
+++ src/main/java/com/project/tb/services/QRCodeServices.java
@@ -19,7 +19,6 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
-
 public class QRCodeServices {
 	static String charset = "UTF-8";
 	static String filePath = "D:\\TicketBoxBack\\rsrc\\qrCodes\\";
